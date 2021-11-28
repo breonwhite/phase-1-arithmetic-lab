6ac2c96d530aea9de4c62c2b6a24555581f93a79
Breon White <bdwhite92@gmail.com>
2021-11-28 16:47:52 -0500

    commit tree c1b9d26beb612cc1d6be9daf7d03c87c967df501

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 32;
+const num4 = 8;
+const multiply = (num1*num2);
 
-// Write your code here
+const random = Math.ceil(Math.random() * 10);
+
+const mod = (num3/num4);
+
+const max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 19.5, 20);
+
